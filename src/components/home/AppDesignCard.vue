2050116dd50ee56ae2bcd858dc7c3a8fb4d28905
Anton Vorobiov <34216721+drageryort@users.noreply.github.com>
2023-03-13 20:57:26 +0200

    pre final. Need decompostion

---
+++ src/components/home/AppDesignCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="card">
+    <picture class="picture">
+      <img src="" alt="">
+    </picture>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "design_card"
+}
+</script>
+
+<style scoped>
+
+</style>
